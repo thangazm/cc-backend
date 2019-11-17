5ef0e52d747b60ce7bf781576a215dbe1e4e019b
thangazm <57315144+thangazm@users.noreply.github.com>
2019-11-17 09:35:58 +0530

    hello world

--- LambdaRest/lambdademo.py
+++ LambdaRest/lambdademo.py
@@ -4,17 +4,8 @@ import json
 # ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
-   number1 = event['Number1']
-   number2 = event['Number2']
-   sum = number1 + number2
-   product = number1 * number2
-   difference = abs(number1 - number2)
-   quotient = number1 / number2
-   return {
-       "Number1": number1,
-       "Number2": number2,
-       "Sum": sum,
-       "Product": product,
-       "Difference": difference,
-       "Quotient": quotient
-   }
+    print('Hello from Lambda!')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
