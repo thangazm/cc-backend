c7173d4168c51bcb098c3b2ee53231997b0da93b
thangazm <57315144+thangazm@users.noreply.github.com>
2019-11-17 09:24:06 +0530

    Functional test

--- LambdaRest/lambdademo.py
+++ LambdaRest/lambdademo.py
@@ -2,6 +2,19 @@ import boto3
 import json
 
 ec2 = boto3.client('ec2')
+
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+   number1 = event['Number1']
+   number2 = event['Number2']
+   sum = number1 + number2
+   product = number1 * number2
+   difference = abs(number1 - number2)
+   quotient = number1 / number2
+   return {
+       "Number1": number1,
+       "Number2": number2,
+       "Sum": sum,
+       "Product": product,
+       "Difference": difference,
+       "Quotient": quotient
+   }
