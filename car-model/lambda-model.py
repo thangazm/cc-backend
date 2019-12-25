6ed5d7e2fe92a72270e889503485f5fa9d21fb0c
thangazm <57315144+thangazm@users.noreply.github.com>
2019-12-25 20:39:49 +0530

    test function method

--- car-model/lambda-model.py
+++ car-model/lambda-model.py
@@ -6,17 +6,21 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
-
-    method = event.httpMethod
+    method = ''
+    # method = event
+    for (key, value) in event.items():
+   # Check if key is even then add pair to new dictionary
+        if key == "httpMethod":
+            method = value
 
     if method == 'GET':
         table = dynamodb.Table('vehicleModel')
         data = table.scan()
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps(data)
-        }
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data)
+    }
     # return response
     # try:
     #     table = dynamodb.Table('vehicleModel')
