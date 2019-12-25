880d9f4f2308f0374a6ca2d7589f67d89b39d57c
thangazm <57315144+thangazm@users.noreply.github.com>
2019-12-25 20:00:48 +0530

    api method test

--- LambdaRest/lambdademo.py
+++ LambdaRest/lambdademo.py
@@ -7,5 +7,5 @@ def lambda_handler(event, context):
     print('Hello from Lambda!')
     return {
         'statusCode': 200,
-        'body': event
+        'body': json.dumps(event)
     }
--- car-model/lambda-search.py
+++ car-model/lambda-search.py
@@ -4,8 +4,21 @@ import json
 
 # ec2 = boto3.client('ec2')
 
+# def lambda_handler(event, context):
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Hello from Lambda!')
+#     }
+
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    
+    try:
+        
+    
+        return {
+            'statusCode': 200,
+            'message':'Success!'}
+    except:
+        return {
+            'statusCode': 400,
+            'body': 'Error, bad request!'}
