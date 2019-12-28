d72c55f1b65c93332a04660e715b6e67fcc62c4e
thangazm <57315144+thangazm@users.noreply.github.com>
2019-12-28 06:45:46 +0530

    function update

--- car-model/lambda-model.py
+++ car-model/lambda-model.py
@@ -16,15 +16,142 @@ def lambda_handler(event, context):
         # }
         # get_models(table)
         query_string_params = event['queryStringParameters']
+        # return {
+        #     'body': json.dumps(query_string_params)
+        # }
         if query_string_params == None:
-            get_models(table)
+            # get_models(table)
+            try:
+
+                data = table.scan()
+        
+                response = {
+                    'statusCode': 200,
+                    'body': json.dumps(data['Items'])
+                }
+        
+                return response
+
+            except:
+                return {
+                    'statusCode': 400,
+                    'body': 'Error, bad request!'
+                    
+                }
         else:
-            for param, value in query_string_params.keys():
-                if param == 'id':
+            for key, value in query_string_params.items():
+                if key == 'id':
                     id = value
-                    get_models_by_id(table, id)
-                if param == 'name':
-                    name = values
+                    
+                    response = table.get_item(
+                        Key={
+                            'model_id': id,
+                        }
+                    )
+                    
+                    item = response['Item']
+                    return {
+                        'body': json.dumps(item)
+                    }
+                if key == 'name':
+                    name = value
+                    data = table.scan()
+                    # print(data['Items'])
+                    for model in data['Items']:
+                        print(model)
+                        for key, value in model.items():
+                            if key == 'model_name' and value == name:
+                                    # result =a {}
+                              return {
+                                  'body': json.dumps(model)
+                              } 
+                              
+                    # response = table.get_item(
+                    #     Key={
+                    #         "model_name": name,
+                    #     }
+                    # )
+                    
+                    # item = response['Item']
+                    # return {
+                    #     'body': json.dumps(result)
+                    # }
+            # print(query_string_params['id'])
+            # return {
+            #         'statusCode': 200,
+            #         'body': json.dumps(query_string_params['id'])
+            #     }
+            # if query_string_params['id'] not None:
+            #     try:
+                    
+            #         response = table.get_item(
+            #             Key={
+            #                 'model_id': query_string_params['id'],
+            #             }
+            #         )
+                    
+            #         item = response['Item']
+            #         return {
+            #             'body': json.dumps(item)
+            #         }
+            #     except:
+                    
+            #         return {
+            #             'statusCode': 400,
+            #             'body': 'Error, bad request!'
+                        
+            #         }
+                
+            # if query_string_params['name'] not None:
+            #     try:
+                    
+            #         response = table.get_item(
+            #             Key={
+            #                 'model_name': query_string_params['name'],
+            #             }
+            #         )
+                    
+            #         item = response['Item']
+            #         return {
+            #             'body': json.dumps(item)
+            #         }
+            #     except:
+            #         return {
+            #             'statusCode': 400,
+            #             'body': 'Error, bad request!'
+                        
+            #         }
+            # for param, value in query_string_params.keys():
+                
+                # if param == 'id':
+                #     return {
+                #         'body': json.dumps(value)
+                #     }
+                #     id = value
+                    # get_models_by_id(table, id)
+                    # try:
+                    
+                    # response = table.get_item(
+                    #     Key={
+                    #         'model_id': id,
+                    #     }
+                    # )
+                    # item = response['Item']
+                    # return json.dumps(item, indent=4, cls=DecimalEncoder)
+                        # response = {
+                        #     'statusCode': 200,
+                        #     'body': json.dumps(data['Items'])
+                        # }
+                
+                        # return response
+                
+                    # except ClientError as e:
+                    #     return e.response['Error']['Message']
+                    # else:
+                    #     item = response['Item']
+                    #     return json.dumps(item, indent=4, cls=DecimalEncoder)
+                # if param == 'name':
+                #     name = values
     if method == 'POST':
         try:
             jsondata = ast.literal_eval(event['body'])
