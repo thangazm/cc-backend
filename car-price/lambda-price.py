818114e3c7a6885e1644a78275b665d8c48097d3
thangazm <57315144+thangazm@users.noreply.github.com>
2019-12-29 14:57:59 +0530

    complete get price list with models

--- car-price/lambda-price.py
+++ car-price/lambda-price.py
@@ -5,20 +5,36 @@
 import json
 import ast
 import boto3
-import uuid
 from boto3 import client as boto3_client
+from collections import defaultdict
+from operator import itemgetter
+from decimal import Decimal
 
 lambda_client = boto3_client('lambda', region_name="us-east-1")
 
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
+lambda_client = boto3_client('lambda', region_name="us-east-1")
+
+# convert decimal types of objects to serialize the json output
+class fakefloat(float):
+    def __init__(self, value):
+        self._value = value
+    def __repr__(self):
+        return str(self._value)
+
+def defaultencode(o):
+    if isinstance(o, Decimal):
+        # Subclass float with custom repr?
+        return fakefloat(o)
+    raise TypeError(repr(o) + " is not JSON serializable")
+
 
 def lambda_handler(event, context):
     method = ''
     method = event['httpMethod']
     table = dynamodb.Table('price')
 
-    # if method == 'GET':
     if method == 'POST':
         try:
             post_data = event['body']
@@ -35,26 +51,39 @@ def lambda_handler(event, context):
                 'statusCode': 400,
                 'body': 'Error, bad request!'
             }
-    if method == 'GET':
-        data = table.scan()
 
-         response = lambda_client.invoke(
-            FunctionName="cc-lambda-cf-stack-carLambdaPrice-J8KNKVM1XEU2",
-            InvocationType='RequestResponse',
-        )
-        return {
-            'statusCode': 200,
-            'body': json.dumps(response)
-        }
-        # try:
+    if method == 'GET':
+        try:
+            data = table.scan()
+            payload = {}
+            payload['httpMethod'] = 'GET'
+            payload['queryStringParameters'] = None
+
+            # call lambda function to get all the models available
+            response_model = lambda_client.invoke(
+                FunctionName="cc-lambda-cf-stack-carLambdaModel-J61DSV3WJ0O6",
+                InvocationType='RequestResponse',
+                Payload= json.dumps(payload)
+            )
             
-        #     return {
-        #         'statusCode': 200,
-        #         'body': json.dumps(value)
-        #     }
-        # except:
-        #     return {
-        #         'statusCode': 400,
-        #         'body': 'Error, bad request!'
-        #     }
+            res_json = json.loads(response_model['Payload'].read().decode("utf-8"))
+            models = json.loads(res_json['body']) # returns a list of all models
+            price_list = data['Items']
+
+            d = defaultdict(dict)
+            for l in (price_list, models):
+                for elem in l:
+                    d[elem['model_id']].update(elem)
+
+            combine_list = sorted(d.values(), key=itemgetter("model_id"))
+
+            return {
+                'statusCode': 200,
+                'body': json.dumps(combine_list, default=defaultencode)
+            }
+        except:
+            return {
+                'statusCode': 400,
+                'body': 'Error, bad request!'
+            }
         
